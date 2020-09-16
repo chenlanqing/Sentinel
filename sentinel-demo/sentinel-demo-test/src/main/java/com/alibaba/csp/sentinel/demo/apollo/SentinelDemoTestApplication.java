325bbc92ff62d6ef384739b5fb2f39799a55cc5d
chenlanqing <743633145@qq.com>
2020-09-16 21:12:39 +0800

    add test

---
+++ sentinel-demo/sentinel-demo-test/src/main/java/com/alibaba/csp/sentinel/demo/apollo/SentinelDemoTestApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.csp.sentinel.demo.apollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author QingFan 2020-09-16
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class SentinelDemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelDemoTestApplication.class, args);
+    }
+}
---
+++ sentinel-demo/sentinel-demo-test/src/main/java/com/alibaba/csp/sentinel/demo/apollo/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.alibaba.csp.sentinel.demo.apollo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author QingFan 2020-09-16
+ * @version 1.0.0
+ */
+@RestController
+public class IndexController {
+
+}
